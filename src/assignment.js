6acd4065327adeadc407e345cdd1e6e37d2d5947
Ayomide Kehinde <105981739+Kikeay@users.noreply.github.com>
2022-07-12 17:25:21 +0000

    final commit

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,15 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
-}
-// assignment.sumOfNumbers = sumOfNumbers;
+    let sum = 0
+  
+    for(let i = 0; i < arrayOfNumbers.length; i++) {
+      sum += arrayOfNumbers[i]
+    }
+    return sum
+    // return arrayOfNumbers.length;
+  }
+assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -28,9 +34,15 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
-}
-// assignment.countEvenNumbers = countEvenNumbers;
+    let evenNumbers = []
+    for (let i = 0; i < arrayOfNumbers.length; i++)   { 
+      if(arrayOfNumbers[i] % 2 === 0) {
+        evenNumbers.push(arrayOfNumbers[i])
+      }
+    }
+    return evenNumbers.length;
+  }
+assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
